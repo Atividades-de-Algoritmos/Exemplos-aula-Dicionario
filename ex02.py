6fe02de4f76f83bfc6fac488e1432bc3a8b7e540
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-29 10:07:48 -0300

    Create ex02.py

---
+++ ex02.py
@@ -0,0 +1,29 @@
+#
+# Autores:
+# Michel Silva
+# Emanuel Franklyn
+# Carlos Eduardo
+#
+# data: 26/06/2022
+#
+# Exercício 2: obtenha a chave de um valor mínimo do seguinte dicionário
+# d1 = {'Física': 82, Matemática': 65, 'História': 75}
+# entrada de dados
+#
+
+# dicionário d1 - dicionário com valores inteiros
+d1 = {
+  'Física': 82,
+  'Matemática': 65,
+  'Historia': 75}
+
+# 1 - verificar se um determinado valor existe em um dicionário.
+t = min(d1.values()) # obtém o valor mínimo do dicionário d1
+print(t) # imprime o valor mínimo do dicionário d1 (65)
+
+for i in d1: # percorre o dicionário d1 e verifica se o valor mínimo existe em cada chave do dicionário d1
+     if (d1[i] == t): # se o valor mínimo existe em cada chave do dicionário d1 então imprime a chave que contém o valor mínimo
+         print(i) # imprime a chave que contém o valor mínimo do dicionário d1
+         break # para o loop
+
+print("final do programa")
