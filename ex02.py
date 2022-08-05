5cabe56dabc1ce85a37477e4736610a1d5ba75a4
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-08-05 18:56:08 -0300

    Obtendo valor mínimo ⬇

--- ex02.py
+++ ex02.py
@@ -4,26 +4,39 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 29/06/2022
+# data: 30/07/2022
 #
-# Exercício 2: obtenha a chave de um valor mínimo do seguinte dicionário
+# Exercício 2: Obtenha a chave de um valor mínimo do seguinte dicionário;
 # d1 = {'Física': 82, Matemática': 65, 'História': 75}
-# entrada de dados
-#
 
-# dicionário d1 - dicionário com valores inteiros
-d1 = {
+# -- Entrada de dados --
+
+notas = {
   'Física': 82,
-  'Matemática': 65,
-  'Historia': 75}
+  'Matemática': 65, # Dicionário d1 - Dicionário com valores inteiros para cada matéria
+  'Historia': 75
+  }
 
-# 1 - verificar se um determinado valor existe em um dicionário.
-t = min(d1.values()) # obtém o valor mínimo do dicionário d1
-print(t) # imprime o valor mínimo do dicionário d1 (65)
+# -- Processamento de dados --
 
-for i in d1: # percorre o dicionário d1 e verifica se o valor mínimo existe em cada chave do dicionário d1
-     if (d1[i] == t): # se o valor mínimo existe em cada chave do dicionário d1 então imprime a chave que contém o valor mínimo
-         print(i) # imprime a chave que contém o valor mínimo do dicionário d1
-         break # para o loop
+# // Com a função min()
 
-print("final do programa")
+menor_nota = min(notas.values()) # Obtendo o valor mínimo do dicionário
+
+# // Sem a função min() Vejam como as funções facilitam nossas vidas xD
+
+for indice, valor in enumerate(notas.values()): # Usei o enumerate para criar uma tupla com os valores e os indíces
+  if indice == 0: # Quero que quando for o início ele defina o primeiro valor como a menor nota
+    menor_nota = valor # Definindo a menor nota como o primeiro valor do dicionário
+
+  if valor < menor_nota: # Se o novo valor do dicinário for menor que o anterior, o novo valor deve se tornar a menor nota
+    menor_nota = valor # Definindo a menor nota como um novo valor.
+  
+  else: # Se não, vai executar o pass e segue percorrendo o dicionário até os elementos chegarem ao fim.
+    pass # Ignora um ciclo de código, e passa uma linha.
+
+# -- Saída de dados --
+
+print(f'Dicionário: {notas}')
+print(f'\nMenor valor: {menor_nota}') # Imprimindo o valor mínimo do dicionário
+print("\nFim do programa") # Imprimindo ao usuário que o programa terminou
